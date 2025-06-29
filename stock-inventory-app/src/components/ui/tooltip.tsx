32d826768ecac8b823b3fffb9c5cae926af129b2
tmpl1999 <tamaht6@gmail.com>
2025-06-29 22:30:52 +0800

    Create tooltip.tsx

---
+++ stock-inventory-app/src/components/ui/tooltip.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+import * as TooltipPrimitive from '@radix-ui/react-tooltip';
+
+export {
+  TooltipPrimitive as Tooltip,
+  TooltipPrimitive as TooltipContent,
+  TooltipPrimitive as TooltipTrigger,
+  TooltipPrimitive as TooltipProvider,
+};
